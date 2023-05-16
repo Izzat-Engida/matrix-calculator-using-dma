716a739c89f8733bfb4370ddc7fa5b8d78cab782
Izzat <124386339+Izzat-Engida@users.noreply.github.com>
2023-05-16 09:02:51 +0300

    Update matrix_dma.cpp

--- matrix_dma.cpp
+++ matrix_dma.cpp
@@ -5,6 +5,13 @@ int main() {
     int** arry;
     int** ptr;
     int row, column;
+    int choice,coun1=0,coun2=0;
+    cout<<"welcome choose from the following to calclulate: "<<endl;
+    cout<<"1.Sum of two matrices\n";
+    cout<<"2. transpose of a matrix\n";
+    cout<<"3. multiplication of two matrice\n";
+    cin>>choice;
+if(choice==1){
     cout << "enter the row: ";
     cin >> row;
     cout << "enter the column: ";
@@ -53,8 +60,83 @@ int main() {
             cout << ptr[i][j] << " ";
         }
         cout << endl;
+    }}
+    else if(choice==2){ 
+    cout << "enter the row: ";
+    cin >> row;
+    cout << "enter the column: ";
+    cin >> column;
+    cout << "enter for the first matrix" << endl;
+    arry = new int* [row];
+    for (int i = 0; i < row; i++) {
+        arry[i] = new int[column];
+    }
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            cout << "enter :" << i + 1 << " " << j + 1 << ": ";
+            cin >> arry[i][j];
+        }
+    }cout<<"the transpose is"<<endl;
+    for(int i=0;i<column;i++){
+        for(int j=0;j<row;j++){
+          cout<<arry[j][i]<<" ";
+            }cout<<endl;
+        }
+    }
+ else if(choice==3){
+
+cout << "enter the row: ";
+    cin >> row;
+    cout << "enter the column: ";
+    cin >> column;
+    cout << "enter for the first matrix" << endl;
+    arry = new int* [row];
+    for (int i = 0; i < row; i++) {
+        arry[i] = new int[column];
+    }
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            cout << "enter :" << i + 1 << " " << j + 1 << ": ";
+            cin >> arry[i][j];
+        }
+    }
+    cout << endl
+        << "enter for the second matrix " << endl;
+    pdr = new int* [row];
+    for (int i = 0; i < row; i++) {
+        pdr[i] = new int[column];
+    }
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            cout << "enter, " << i + 1 << "," << j + 1 << " : ";
+            cin >> pdr[i][j];
+        }
+    }
+    ptr = new int* [row];
+    for (int i = 0; i < row; i++) {
+        ptr[i] = new int[column];
     }
 
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            ptr[i][j] = 0;
+            for(int k=0;k<column;k++){
+                ptr[i][j]+=arry[i][k]*pdr[k][j];
+            }
+        }
+    } for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            cout<<ptr[i][j]<<" ";
+        }cout<<endl;
+    } 
+
+    }
+for(int i=0;i<row;i++){
+    delete[]arry[i];
+    delete[]pdr[i];
+    delete[]ptr[i];
+    
+}
     delete[] arry;
     delete[] pdr;
     delete[] ptr;
